c83ac1181ad1657e090609767668e9e99e7a2595
sandeepyadavkec <sandeepyadav.kec@gmail.com>
2018-02-27 16:14:51 +1100

    Completed
    This is the final version, fully working, basic neural network, trained to learn XOR operation.

--- BasicNeuralNetJava/src/neural/Neuron.java
+++ BasicNeuralNetJava/src/neural/Neuron.java
@@ -3,40 +3,90 @@ package neural;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.lang.Math;
 
 public class Neuron {
 	
 	List<Double> m_outputWeights = new ArrayList<Double>();
 	List<Double> m_deltaWeights = new ArrayList<Double>();
-	double m_outputVal;
+	private double m_outputVal;
 	Random rand = new Random();
+	int m_myIndex;
+	double m_gradient;
+	static double eta = 0.15;
+	static double alpha = 0.5;
 	
-	public Neuron(int numOutputs, int neuronNum){
-				
+	public Neuron(int numOutputs, int myIndex){
+		
+		//Initialize output and delta weights
 		for(int c=0; c < numOutputs; c++) {
 			m_outputWeights.add(c, rand.nextDouble());
-			System.out.println("Weight, index: "+m_outputWeights.get(c)+", "+c);
+			m_deltaWeights.add(0.0);			
 		}
+		m_myIndex = myIndex;		
 	}
 
 	public void setOutputVal(double val) {
-		// TODO Auto-generated method stub
+		// set output values
 		m_outputVal = val;
 	}
 	
 	public double getOutputVal() {
-		// TODO Auto-generated method stub
+		// get output values
 		return m_outputVal;
 	}
 
 	public void feedForward(List<Neuron> prevLayer) {
-		// TODO Auto-generated method stub
+		// aggregated summation
 		double sum = 0.0;
 		
 		for(int n=0; n<prevLayer.size(); n++) {
-			
+			sum += prevLayer.get(n).getOutputVal() * 
+			prevLayer.get(n).m_outputWeights.get(m_myIndex);
 		}
+		m_outputVal=transferFunction(sum);
 		
+	}
+
+	private double transferFunction(double x) {
+		//return 1.0 - x * x;
+		return Math.tanh(x);
+	}
+	
+	private double transferFunctionDerivative(double x) {
+		// tanh derivative
+		return 1.0 - x * x;		
+	}
+
+	public void calcOutputGradients(Double targetVal) {		
+		double delta = targetVal - m_outputVal;
+		m_gradient = delta * transferFunctionDerivative(m_outputVal);
+	}
+
+	public void calcHiddenGradients(List<Neuron> nextLayer) {		
+		double dow = sumDOW(nextLayer);
+		m_gradient = dow * transferFunctionDerivative(m_outputVal);
+		}
+
+	private double sumDOW(List<Neuron> nextLayer) {		
+		double sum = 0.0;		
+		for(int n=0; n < nextLayer.size()-1; n++) {
+			sum += m_outputWeights.get(n).doubleValue() * nextLayer.get(n).m_gradient;
+		}
+		return sum;
+	}
+
+	public void updateInputWeights(List<Neuron> prevLayer) {		
+		for(int n=0; n < prevLayer.size(); n++) {
+			Neuron neuron = prevLayer.get(n);
+			double oldDeltaWeight = neuron.m_deltaWeights.get(m_myIndex).doubleValue();
+			
+			//Individual input, magnified by the gradient and the training rate
+			double newDeltaWeight = eta * neuron.getOutputVal() * m_gradient + alpha * oldDeltaWeight;
+			
+			neuron.m_deltaWeights.set(m_myIndex, newDeltaWeight);
+			neuron.m_outputWeights.set(m_myIndex, neuron.m_outputWeights.get(m_myIndex)+newDeltaWeight);
+		}
 		
 	}
 
--- BasicNeuralNetJava/src/test/Test.java
+++ BasicNeuralNetJava/src/test/Test.java
@@ -2,32 +2,66 @@ package test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import neural.Net;
 
 public class Test {
 
 	private static List<Double> inputVals = new ArrayList<Double>();
-	private static List<Double> outputVals = new ArrayList<Double>();
+	private static List<Double> targetVals = new ArrayList<Double>();
 		
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+				
 		List<Double> resultVals = null;
 		List<Integer> topology = new ArrayList<Integer>();
-		topology.add(3);
+					
 		topology.add(2);
+		topology.add(4);
 		topology.add(1);
 		Net myNet = new Net(topology);
+		Random rand = new Random();
 		
-		myNet.feedForward(inputVals);
-		myNet.backProp(outputVals);
-		myNet.getResults(resultVals);
+		for(int i =0; i<20000; i++) {
+			System.out.println("Pass: "+i);
+			inputVals.clear();
+			targetVals.clear();
+			int a = (int)(2*rand.nextDouble());
+			inputVals.add((double)a);
+			int b = (int)(2*rand.nextDouble());
+			inputVals.add((double)b);
+			System.out.println("in: "+(double)a+"\t"+(double)b);
+			
+			//Pass inputs
+			myNet.feedForward(inputVals);
+			
+			//Get results
+			resultVals = myNet.getResults();
+			System.out.println("result: "+resultVals.get(0).doubleValue());
+						
+			int c = logicalXOR(a,b);
+			targetVals.add((double)c);
+			System.out.println("target: "+(double)c);
+			
+			//Train the net
+			myNet.backProp(targetVals);
+			
+			//Un-comment for debugging
+			//myNet.dumpResults();
+		}
+		
+	}
 
+	public static int logicalXOR(int a, int b) {
+		if((a==0 && b==0) || (a==1 && b==1)) {
+			return 0;
+		}
+		return 1;
+			
 	}
 
 }
